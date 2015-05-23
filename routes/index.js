18b070151839055a61aed8e58f3e449845338676
Kikurage <y.kikura@gmail.com>
2015-05-23 17:12:57 +0900

    update view

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ var request = require('request');
 var express = require('express');
 var router = express.Router();
 var User = require('../model/user.js');
+var Video = require('../model/video.js');
 var Session = require('../model/session.js');
+var ObjectId = require('mongoose').Types.ObjectId;
 var crypto = require("crypto");
 var FB_ENDPOINT = 'https://graph.facebook.com/v2.3'
+var videoAPIRouter = require('./api_video.js');
 
 function sendErr(err, res, code) {
 	console.err(err);
@@ -19,10 +22,65 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/index.html', function(req, res, next) {
-	res.render('index', {
-		title: 'Express',
-		user: req.session.user
-	});
+	var n = req.query.page || 1,
+		MAX_IN_A_PAGE = 50;
+
+	Video.find({})
+		.skip((n - 1) * MAX_IN_A_PAGE)
+		.limit(MAX_IN_A_PAGE)
+		.exec(function(err, videos) {
+			if (err) return sendErr(res);
+
+			Promise.all(videos.map(function(video) {
+					return new Promise(function(resolve, reject) {
+						User.findById(new ObjectId(video.userId), function(err, user) {
+							if (err) {
+								return reject(err)
+							}
+
+							return resolve(user);
+						});
+					});
+				}))
+				.then(function(users) {
+					return new Promise(function(resolve, reject) {
+						var videoObjects = videos.map(function(video, i) {
+							return {
+								title: 'タイトルはまだ未定',
+								videoId: video._id.toString(),
+								thumbnail: 'サムネなんてなかった！！！！',
+								user: {
+									userId: users[i]._id.toString(),
+									name: users[i].name
+								}
+							}
+						});
+
+						Video.count({}, function(err, count) {
+							if (err) reject(err);
+							if (Math.ceil(count / MAX_IN_A_PAGE) < n) reject(err);
+
+							resolve({
+								current: n,
+								max: Math.ceil(count / MAX_IN_A_PAGE),
+								videos: videoObjects
+							});
+						});
+					});
+				})
+				.then(function(videoData) {
+					res.render('index', {
+						title: 'Express',
+						current: videoData.current,
+						max: videoData.max,
+						videos: videoData.videos,
+						user: req.session.user
+					});
+				})
+				.catch(function(err) {
+					sendErr(res, err);
+				});
+		});
 });
 
 router.get('/video/:videoId', function(req, res, next) {
